4f82289c1265acf685053c121e66a2b8e66bda10
Ade Viankakrisna Fadlil <viankakrisna@gmail.com>
2017-09-09 22:24:16 +0700

    tweak google maps

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -1,14 +1,38 @@
-import { combineReducers } from 'redux';
-import user from './user';
+import { combineReducers } from "redux";
+import user from "./user";
 
 const rootReducer = combineReducers({
   user,
-  locale: (state = 'en-US', action) =>
-    action.type === 'SET_LOCALE' ? action.payload : state,
+  locale: (state = "en-US", action) =>
+    action.type === "SET_LOCALE" ? action.payload : state,
   localeLoaded: (state = false, action) =>
-    action.type === 'LOCALE_LOADED'
+    action.type === "LOCALE_LOADED"
       ? true
-      : action.type === 'LOAD_LOCALE' ? false : state,
+      : action.type === "LOAD_LOCALE" ? false : state,
+  bop: (
+    state = {
+      location: {
+        bounds: null,
+        center: {
+          lat: 47.6205588,
+          lng: -122.3212725
+        },
+        markers: []
+      }
+    },
+    action
+  ) => {
+    if (action.type === "BECOME_OUR_PHOTOGRAPHER_PLACES_CHANGED") {
+      return {
+        ...state,
+        location: {
+          ...state.location,
+          ...action.payload
+        }
+      };
+    }
+    return state;
+  }
 });
 
 export default rootReducer;
