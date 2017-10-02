3f24fdc5cbeae389e93f7fa350b3c2f41c465576
Agung Surya Bangsa <agung.surya@kumparan.com>
2017-10-03 00:52:39 +0700

    Rework progress

--- src/store/reducers/photographerServiceInfoReducersStep2.js
+++ src/store/reducers/photographerServiceInfoReducersStep2.js
@@ -1,13 +1,37 @@
 import camelCase from 'lodash/camelCase';
 
-const initialState = null;
+const initialState = {
+  loading: false,
+  loaded: false,
+  percentages: [],
+};
 
 const photographerServiceInfoStep2 = (state = initialState, action) => {
   switch (action.type) {
     case 'BECOME_OUR_PHOTOGRAPHER_PRICING_CHANGED':
       return {
         ...state,
+        loading: false,
+        loaded: true,
         ...action.payload,
+      };
+    case 'SUBMIT_UPLOAD_PORTFOLIO':
+      return {
+        loading: true,
+        loaded: false,
+        percentages: action.percentages,
+      };
+    case 'SUBMIT_UPLOAD_PORTFOLIO_SUCCESS':
+      return {
+        loading: false,
+        loaded: true,
+        ...state.percentages,
+      };
+    case 'SUBMIT_UPLOAD_PORTFOLIO_ERROR':
+      return {
+        loading: false,
+        loaded: true,
+        error: action.error,
       };
     default:
       return state;
