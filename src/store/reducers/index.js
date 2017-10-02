b5c1d24a8cb3bfb12d75870eefcf9b40801494bb
Agung Surya Bangsa <agung.surya@kumparan.com>
2017-10-02 13:45:30 +0700

    Finish

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -2,12 +2,14 @@ import { combineReducers } from 'redux';
 import { userAuth, userSignup } from './userReducers';
 import { userInitProfile } from './userInitProfileReducers';
 import photographerServiceInfo from './photographerServiceInfoReducers';
+import photographerServiceInfoStep2 from './photographerServiceInfoReducersStep2';
 
 const rootReducer = combineReducers({
   userAuth,
   userSignup,
   userInitProfile,
   photographerServiceInfo,
+  photographerServiceInfoStep2,
   locale: (state = 'en-US', action) =>
     action.type === 'SET_LOCALE' ? action.payload : state,
   localeLoaded: (state = false, action) => {
