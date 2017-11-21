e05d4dc8172dae4eeb26576df8ed28657c83fc72
Muhamad Risman <real.risman@gmail.com>
2017-11-21 16:28:05 +0700

    Update component user camera equipment

--- src/components/User/CameraEquipment.js
+++ src/components/User/CameraEquipment.js
@@ -1,12 +1,30 @@
 import React, { Component } from 'react'
-import { Form, FormGroup, Col, ControlLabel, FormControl } from 'react-bootstrap'
+import { Form, FormGroup, Col, ControlLabel, FormControl, Button } from 'react-bootstrap'
 
 export default class CameraEquipment extends Component {
   render() {
-    
+
     return (
       <Form horizontal>
+        <FormGroup>
+          <Col componentClass={ControlLabel} sm={2}>
+            Body
+          </Col>
+          <Col sm={6}>
+            <FormControl type="text"/>
+            <Button style={{float:'right'}}>Add More</Button>
+          </Col>
+        </FormGroup>
 
+        <FormGroup>
+          <Col componentClass={ControlLabel} sm={2}>
+            Lens
+          </Col>
+          <Col sm={6}>
+            <FormControl type="text"/>
+            <Button style={{float:'right'}}>Add More</Button>
+          </Col>
+        </FormGroup>
       </Form>
     );
   }
