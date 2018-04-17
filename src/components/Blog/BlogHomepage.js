d142f3121b4619dd163478d8fa5739dea432fad4
Muhamad Tohir <maztohir@gmail.com>
2018-04-17 10:03:26 +0700

    designing blog

--- src/components/Blog/BlogHomepage.js
+++ src/components/Blog/BlogHomepage.js
@@ -6,7 +6,55 @@ class BlogHomepage extends Component {
   render() {
     return (
       <Page>
-        <div>Write blog homepage markup</div>
+        <div className="container">
+        	<img className="blog-header" src="/images/blog/header.png"></img>
+        	<div className="blog-body">
+	        	<div className="blog-tab">
+		        	<span className="tab-item active">RECENT</span>
+		        	<span className="tab-item">POPULAR</span>
+		        </div>
+		        <div className="row" style={{marginTop:'34px'}}>
+		        	<div className="blog-item col-xs-12 col-sm-6 col-md-4">
+		        		<img className="blog-thumbnail" src="/images/blog/blog1.png"></img>
+		        		<div className="topic">LOCATION</div>
+		        		<div className="title">BALI</div>
+		        		<div className="subtitle">THE ISLAND OF GOD</div>
+		        	</div>
+		        	<div className="blog-item col-xs-12 col-sm-6 col-md-4">
+		        		<img className="blog-thumbnail" src="/images/blog/blog2.png"></img>
+		        		<div className="topic">LOCATION</div>
+		        		<div className="title">BALI</div>
+		        		<div className="subtitle">THE ISLAND OF GOD</div>
+		        	</div>
+		        	<div className="blog-item col-xs-12 col-sm-6 col-md-4">
+		        		<img className="blog-thumbnail" src="/images/blog/blog3.png"></img>
+		        		<div className="topic">LOCATION</div>
+		        		<div className="title">BALI</div>
+		        		<div className="subtitle">THE ISLAND OF GOD</div>
+		        	</div>
+		        </div>
+		        <div className="row" style={{marginTop:'34px'}}>
+		        	<div className="blog-item col-xs-12 col-sm-6 col-md-4">
+		        		<img className="blog-thumbnail" src="/images/blog/blog4.png"></img>
+		        		<div className="topic">LOCATION</div>
+		        		<div className="title">BALI</div>
+		        		<div className="subtitle">THE ISLAND OF GOD</div>
+		        	</div>
+		        	<div className="blog-item col-xs-12 col-sm-6 col-md-4">
+		        		<img className="blog-thumbnail" src="/images/blog/blog3.png"></img>
+		        		<div className="topic">LOCATION</div>
+		        		<div className="title">BALI</div>
+		        		<div className="subtitle">THE ISLAND OF GOD</div>
+		        	</div>
+		        	<div className="blog-item col-xs-12 col-sm-6 col-md-4">
+		        		<img className="blog-thumbnail" src="/images/blog/blog2.png"></img>
+		        		<div className="topic">LOCATION</div>
+		        		<div className="title">BALI</div>
+		        		<div className="subtitle">THE ISLAND OF GOD</div>
+		        	</div>
+		        </div>
+	        </div>
+        </div>
       </Page>
     )
   }
